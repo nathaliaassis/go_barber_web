cd929fd149a1fc98cab886eb540c87c658b14d39
Nathália <nathaliamassis@gmail.com>
2020-10-16 20:11:22 -0300

    authContext hook

--- src/hooks/AuthContext.tsx
+++ src/hooks/AuthContext.tsx
@@ -13,6 +13,7 @@ interface SignInCredentials {
 interface AuthContextData {
   user: object;
   signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
 }
 const AuthContext = createContext<AuthContextData>(
   {} as AuthContextData,
@@ -44,8 +45,15 @@ export const AuthProvider: React.FC = ({ children }) => {
     })
   }, []);
 
+  const signOut = useCallback(() => {
+    const token = localStorage.removeItem('@GoBarber:token');
+    const user = localStorage.removeItem('@GoBarber:user');
+
+    setData({} as StorageData);
+  }, [])
+
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
